6603e204be357b6b92b2ca0180fbc235b1558248
mansoull <mansoul96@gmail.com>
2017-05-16 12:39:43 +0800

    next commit

--- app/src/main/java/systmorder/apporder/OwnerMainActivity.java
+++ app/src/main/java/systmorder/apporder/OwnerMainActivity.java
@@ -1,23 +1,15 @@
 package systmorder.apporder;
 
 import android.content.Context;
-import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.IdRes;
 import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentTransaction;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.EditText;
-import android.widget.Toast;
 
 import com.roughike.bottombar.BottomBar;
 import com.roughike.bottombar.OnMenuTabClickListener;
-
-import static systmorder.apporder.R.id.btmBarDetails;
 
 
 public class OwnerMainActivity extends AppCompatActivity implements OnMenuTabClickListener {
@@ -42,11 +34,30 @@ public class OwnerMainActivity extends AppCompatActivity implements OnMenuTabCli
 
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
 
-        if (menuItemId == R.id.btmBarDetails){
+        if (menuItemId == R.id.btmBarDashboard){
+
+            OwnerDashboardTab fragOwnerDashboardTab = new OwnerDashboardTab();
+            transaction.replace(R.id.owner_activity_main, fragOwnerDashboardTab);
+            transaction.commit();
+
+        } else if (menuItemId == R.id.btmBarDetails){
 
             OwnerMenuTab fragOwnerMenuTab = new OwnerMenuTab();
             transaction.replace(R.id.owner_activity_main, fragOwnerMenuTab);
             transaction.commit();
+
+        } else if (menuItemId == R.id.btmBarUser){
+
+            OwnerUserTab fragOwnerUserTab = new OwnerUserTab();
+            transaction.replace(R.id.owner_activity_main, fragOwnerUserTab);
+            transaction.commit();
+
+        } else if (menuItemId == R.id.btmBarSetting){
+
+            OwnerSettingTab fragOwnerSettingTab = new OwnerSettingTab();
+            transaction.replace(R.id.owner_activity_main, fragOwnerSettingTab);
+            transaction.commit();
+
         }
 
     }
