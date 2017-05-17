70ef3efbb3e09f03d5e2d01878a153c010f2f711
mansoull <mansoul96@gmail.com>
2017-05-17 17:12:53 +0800

    next commit

--- app/src/main/java/systmorder/apporder/AllRegisterActivity.java
+++ app/src/main/java/systmorder/apporder/AllRegisterActivity.java
@@ -1,5 +1,6 @@
 package systmorder.apporder;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -11,17 +12,31 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.HashMap;
 
 
 public class AllRegisterActivity extends AppCompatActivity {
 
     private EditText edtEmailReg, edtPsswrdReg, edtName;
     private Button btnLoginReg;
+    private FirebaseAuth firebaseAuth;
+    private DatabaseReference databaseReference;
+    private ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.all_activity_register);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        databaseReference = FirebaseDatabase.getInstance().getReference().child("tblUser");
 
         edtEmailReg = (EditText) findViewById(R.id.edtEmailReg);
         edtPsswrdReg = (EditText) findViewById(R.id.edtPsswrdReg);
@@ -32,7 +47,53 @@ public class AllRegisterActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
+                progressDialog.setMessage("Storing Data...");
+                progressDialog.show();
 
+                final String strUserEmail = edtEmailReg.getText().toString().trim();
+                final String strUserPass = edtPsswrdReg.getText().toString().trim();
+                final String strUserName = edtName.getText().toString().trim();
+
+                if (TextUtils.isEmpty(strUserEmail) && TextUtils.isEmpty(strUserPass) && TextUtils.isEmpty(strUserName)){
+
+                    Toast.makeText(getApplicationContext(), "Please fill all the empty", Toast.LENGTH_SHORT).show();
+
+                } else if (TextUtils.isEmpty(strUserEmail)){
+
+                    Toast.makeText(getApplicationContext(), "Please enter your email", Toast.LENGTH_SHORT).show();
+                    return;
+
+                } else if (TextUtils.isEmpty(strUserPass)){
+
+                    Toast.makeText(getApplicationContext(), "Please enter your password", Toast.LENGTH_SHORT).show();
+                    return;
+
+                } else if (TextUtils.isEmpty(strUserName)){
+
+                    Toast.makeText(getApplicationContext(), "Please enter your name", Toast.LENGTH_SHORT).show();
+                    return;
+
+                }
+
+                firebaseAuth.createUserWithEmailAndPassword(strUserEmail, strUserPass).addOnCompleteListener(AllRegisterActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (!task.isSuccessful()){
+
+                            progressDialog.dismiss();
+                            Toast.makeText(AllRegisterActivity.this, "Register failed" + task.getException(), Toast.LENGTH_SHORT).show();
+
+                        } else {
+
+                            startActivity(new Intent(AllRegisterActivity.this, AllLoginActivity.class));
+                            finish();
+//                            HashMap<String, String> dataMap = new HashMap<String, String>();
+//                            dataMap.put("userEmail", strUserEmail);
+//                            dataMap.put("userPass", strUserPass);
+//                            dataMap.put("userName", strUserName);
+                        }
+                    }
+                });
             }
         });
     }
