fa5a02226d43ec13e356eb692da6579d7aa82ca9
mansoull <mansoul96@gmail.com>
2017-06-09 12:23:36 +0800

    next commit

--- app/src/main/java/systmorder/apporder/StaffChooseActivity.java
+++ app/src/main/java/systmorder/apporder/StaffChooseActivity.java
@@ -4,23 +4,23 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
 import android.view.View;
 import android.widget.Button;
 
 
 public class StaffChooseActivity extends AppCompatActivity {
 
-    private Button btnStaffGoToStaff, btnStaffGoToCust;
+    private Button btnToCust;
+    private CardView clickToCashier, clickToKitchen;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.staff_activity_choosing);
 
-        btnStaffGoToStaff = (Button) findViewById(R.id.btnStaffGoToStaff);
-        btnStaffGoToCust = (Button) findViewById(R.id.btnStaffGoToCust);
-
-        btnStaffGoToStaff.setOnClickListener(new View.OnClickListener() {
+        clickToCashier = (CardView) findViewById(R.id.clickToCashier);
+        clickToCashier.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(StaffChooseActivity.this, StaffMainActivity.class));
@@ -28,11 +28,23 @@ public class StaffChooseActivity extends AppCompatActivity {
             }
         });
 
-        btnStaffGoToCust.setOnClickListener(new View.OnClickListener() {
+        clickToKitchen = (CardView) findViewById(R.id.clickToKitchen);
+        clickToKitchen.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                startActivity(new Intent(StaffChooseActivity.this, StaffMainActivity.class));
+                finish();
             }
         });
+
+        btnToCust = (Button) findViewById(R.id.btnToCust);
+        btnToCust.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(StaffChooseActivity.this, CustMainActivity.class));
+                finish();
+            }
+        });
+
     }
 }
